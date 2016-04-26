94941dd43f9bd0f881a8af0de0a2ef6273bc3acb
dreizys <dreizys@mail.ru>
2016-04-26 16:23:52 +0300

    Added files via upload

--- dialog.cpp
+++ dialog.cpp
@@ -14,7 +14,7 @@ Dialog::Dialog(QWidget *parent) :
     QList < QLCDNumber* >  lcdnumbers = findChildren<QLCDNumber*>();
 
         foreach ( QLCDNumber * lcdnumber, lcdnumbers) {
-        qDebug() << lcdnumber->objectName();
+        //qDebug() << lcdnumber->objectName();
         lcdnumber->display("-------");
         }
     //ui->temp_lcdNumber_1->display("-------");
@@ -24,6 +24,7 @@ Dialog::Dialog(QWidget *parent) :
     arduino_is_available = false;
     arduino_port_name = "";
     serialBuffer = "";
+    QStringList bufferSplitTemp=serialBuffer.split(",");
 
 
    // qDebug() << "Number of available ports: " << QSerialPortInfo::availablePorts().length();
@@ -82,82 +83,116 @@ Dialog::~Dialog()
 
 void Dialog::readSerial(){
     palette = new QPalette();
-        //qDebug() << "serial port works";
-    serialData = arduino->readAll();
-   // QString temp = QString::fromStdString(serialData.toStdString());
+    //QString serialBuffer;
+        //Get the data from serial, and let MainWindow know it's ready to be collected.
+        while(arduino->canReadLine())
+        {
+            //String for data to go.
+            bool parsedCorrectly = 0;
+            //characters = "";
 
-    //qDebug() << temp;
-    serialBuffer = QString::fromStdString(serialData.toStdString());
-    QStringList bufferSplit=serialBuffer.split(";");
-    qDebug() << bufferSplit[0];
-    qDebug() << bufferSplit[1];
+            //Loop until we find the newline delimiter.
+            do
+            {
+              //Get the line.
+              QByteArray serialData = arduino->readLine();//Line();
+               qDebug() << serialData;
+              //If we have found a new line character in any line, complete the parse.
+              if(QString(serialData).contains('\n'))
+              {
+                 if(QString(serialData) != "\n")
+                  {
+                      serialBuffer += QString(serialData);
+                      serialBuffer.remove(QRegExp("[\\n\\t\\r]"));
+                      parsedCorrectly = 1;
+                      qDebug() << serialBuffer;
+                      QStringList bufferSplit=serialBuffer.split(";");
+                      qDebug() << bufferSplit;
+                      qDebug() << bufferSplit[0];
+                      qDebug() << bufferSplit[1];
+
+                      //Dialog::updateLCD(bufferSplit[1]);
+                     //qDebug() << bufferSplit;
+                     if (bufferSplit[1].toFloat() < 8){
+                      palette->setColor(QPalette::WindowText, Qt::red);
+                     } else{
+                      palette->setColor(QPalette::WindowText, Qt::blue);
+                     }
+
+                     if ( bufferSplit[0] == "1" ){
+                         ui->temp_lcdNumber_1->setPalette(*palette);
+                         ui->temp_lcdNumber_1->display(bufferSplit[1]);
+                           }
+
+                     if ( bufferSplit[0] == "2" ){
+                        ui->temp_lcdNumber_2->setPalette(*palette);
+                        ui->temp_lcdNumber_2->display(bufferSplit[1]);
+                     }
+                     if ( bufferSplit[0] == "3" ){
+                        ui->temp_lcdNumber_3->setPalette(*palette);
+                        ui->temp_lcdNumber_3->display(bufferSplit[1]);
+                     }
+                     if ( bufferSplit[0] == "4" ){
+                        ui->temp_lcdNumber_4->setPalette(*palette);
+                        ui->temp_lcdNumber_4->display(bufferSplit[1]);
+                     }
+                     if ( bufferSplit[0] == "5" ){
+                        ui->temp_lcdNumber_5->setPalette(*palette);
+                        ui->temp_lcdNumber_5->display(bufferSplit[1]);
+                     }
+                     if ( bufferSplit[0] == "6" ){
+                        ui->temp_lcdNumber_6->setPalette(*palette);
+                        ui->temp_lcdNumber_6->display(bufferSplit[1]);
+                     }
+                     if ( bufferSplit[0] == "7" ){
+                        ui->temp_lcdNumber_7->setPalette(*palette);
+                        ui->temp_lcdNumber_7->display(bufferSplit[1]);
+                     }
+                     if ( bufferSplit[0] == "8" ){
+                        ui->temp_lcdNumber_8->setPalette(*palette);
+                        ui->temp_lcdNumber_8->display(bufferSplit[1]);
+                     }
+                     if ( bufferSplit[0] == "9" ){
+                        ui->temp_lcdNumber_9->setPalette(*palette);
+                        ui->temp_lcdNumber_9->display(bufferSplit[1]);
+                     }
+                     if ( bufferSplit[0] == "10" ){
+                        ui->temp_lcdNumber_10->setPalette(*palette);
+                        ui->temp_lcdNumber_10->display(bufferSplit[1]);
+                     }
+                     if ( bufferSplit[0] == "11" ){
+                        ui->temp_lcdNumber_11->setPalette(*palette);
+                        ui->temp_lcdNumber_11->display(bufferSplit[1]);
+                     }
+                     if ( bufferSplit[0] == "12" ){
+                        ui->temp_lcdNumber_12->setPalette(*palette);
+                        ui->temp_lcdNumber_12->display(bufferSplit[1]);
+                     }
+
+                  }
+              }
+              //If we don't find the newline straight away, add the string we got to the characters QString and keep going.
+              else
+                  serialBuffer += QString(serialData);
+            }while(!parsedCorrectly);
+
+            //Save characters to data and emit signal to collect it.
+           // data = characters;
 
 
-                     /*if(serialBuffer.length()<4){
-                        serialData = arduino->readLine();
-                        serialBuffer += QString::fromStdString(serialData.toStdString());
-                         }
-                            // qDebug() << serialBuffer;
-                             else{                                                                //bufferSplit[1] is a good value
-                                QStringList bufferSplit=serialBuffer.split(",");*/
-    // Dialog::updateLCD(bufferSplit[1]);
-    //qDebug() << bufferSplit;
-    if (bufferSplit[1].toFloat() < 8){
-     palette->setColor(QPalette::WindowText, Qt::red);
-    } else{
-     palette->setColor(QPalette::WindowText, Qt::blue);
-    }
+            //Reset characters!
+          serialBuffer = "";
+        }
 
-    if ( bufferSplit[0] == "1" ){
-        ui->temp_lcdNumber_1->setPalette(*palette);
-        ui->temp_lcdNumber_1->display(bufferSplit[1]);
-          }
+                    //instead of multiple if statement
+                     /*for (int i = 0; i < list.size(); ++i) {
+                         if (list.at(i) == "Jane")
+                           cout << "Found Jane at position " << i << endl;
+                     }*/
 
-    if ( bufferSplit[0] == "2" ){
-       ui->temp_lcdNumber_2->setPalette(*palette);
-       ui->temp_lcdNumber_2->display(bufferSplit[1]);
-    }
-    if ( bufferSplit[0] == "3" ){
-       ui->temp_lcdNumber_3->setPalette(*palette);
-       ui->temp_lcdNumber_3->display(bufferSplit[1]);
-    }
-    if ( bufferSplit[0] == "4" ){
-       ui->temp_lcdNumber_4->setPalette(*palette);
-       ui->temp_lcdNumber_4->display(bufferSplit[1]);
-    }
-    if ( bufferSplit[0] == "5" ){
-       ui->temp_lcdNumber_5->setPalette(*palette);
-       ui->temp_lcdNumber_5->display(bufferSplit[1]);
-    }
-    if ( bufferSplit[0] == "6" ){
-       ui->temp_lcdNumber_6->setPalette(*palette);
-       ui->temp_lcdNumber_6->display(bufferSplit[1]);
-    }
-    if ( bufferSplit[0] == "7" ){
-       ui->temp_lcdNumber_7->setPalette(*palette);
-       ui->temp_lcdNumber_7->display(bufferSplit[1]);
-    }
-    if ( bufferSplit[0] == "8" ){
-       ui->temp_lcdNumber_8->setPalette(*palette);
-       ui->temp_lcdNumber_8->display(bufferSplit[1]);
-    }
-    if ( bufferSplit[0] == "9" ){
-       ui->temp_lcdNumber_9->setPalette(*palette);
-       ui->temp_lcdNumber_9->display(bufferSplit[1]);
-    }
-    if ( bufferSplit[0] == "10" ){
-       ui->temp_lcdNumber_10->setPalette(*palette);
-       ui->temp_lcdNumber_10->display(bufferSplit[1]);
-    }
-    if ( bufferSplit[0] == "11" ){
-       ui->temp_lcdNumber_11->setPalette(*palette);
-       ui->temp_lcdNumber_11->display(bufferSplit[1]);
-    }
-    if ( bufferSplit[0] == "12" ){
-       ui->temp_lcdNumber_12->setPalette(*palette);
-       ui->temp_lcdNumber_12->display(bufferSplit[1]);
-    }
-    serialBuffer = "";
+
+
+   // serialBuffer = "";
 }
 
 
@@ -165,7 +200,7 @@ void Dialog::readSerial(){
 
 void Dialog::updateLCD(const QString sensor_reading){
 
-   ui->temp_lcdNumber_1->display(sensor_reading);
+   //ui->temp_lcdNumber_1->display(sensor_reading);
 
 }
 void Dialog::updateContNum(QString command)
@@ -183,4 +218,10 @@ void Dialog::updateContNum(QString command)
 void Dialog::on_pushButton_Send_clicked()
 {
     Dialog::updateContNum(ui->comboBox->currentText());
+    QList < QLCDNumber* >  lcdnumbers = findChildren<QLCDNumber*>();
+
+        foreach ( QLCDNumber * lcdnumber, lcdnumbers) {
+        //qDebug() << lcdnumber->objectName();
+        lcdnumber->display("-------");
+        }
 }
